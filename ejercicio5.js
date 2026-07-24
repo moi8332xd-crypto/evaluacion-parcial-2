89eff22bf9c31ce375062129cf76a2b57a2f1441
moises <moi8332xd@gmail.com>
2026-07-24 09:02:33 -0500

    feature se agrego ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,21 @@
+
+
+for(i=1; i<=20; i++){
+    console.log(i);
+    
+    if(i % 2 == 0){
+        console.log("el numero es par");
+        
+    }else{
+        console.log("es impar");
+        
+    }
+
+
+    
+
+
+    
+    
+
+}
