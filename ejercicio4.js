8f087a160ac62a2423e155b3d572d70c623a7497
moises <moi8332xd@gmail.com>
2026-07-24 08:43:33 -0500

    feature_se agrego el ejercicio4

---
+++ ejercicio4.js
@@ -0,0 +1,18 @@
+//Escribe una función que reciba la edad de una persona y muestre en consola si es "Menor de edad", "Adulto" o "Adulto 
+//mayor" (mayor a 65 años), utilizando if/else. Realiza el commit correspondiente.
+
+let edad = 66;
+
+console.log("ingrese su edad:");
+
+if(edad <= 18){
+    console.log("es menor de edad");
+    
+}else if(edad <= 65){
+    console.log("es adulto");
+    
+}else{
+    console.log("es adulto mayor");
+
+    
+}
