4ea016c097cf5523f06993e110d351d8a4ad44e0
moises <moi8332xd@gmail.com>
2026-07-24 07:56:17 -0500

    feature_se añade ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,21 @@
+let nombreProducto1 = "sandwich";
+let nombreProducto2 = "hotdog";
+let precioProducto1 = 1.50;
+let precioProducto2 = 1.75;
+
+let dispP1 = true;
+let dispP2 = true;
+
+
+console.log("|||Los precios de los productos son:|||");
+console.log("nombre: " + nombreProducto1 + " precio: "+ precioProducto1);
+console.log("nombre: " + nombreProducto2 + " precio: "+ precioProducto2);
+
+
+
+
+
+
+
+
+
